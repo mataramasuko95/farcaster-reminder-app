a46e993cedb651c917968b103f719463b159c44e
MataramaSuko <batuhanpolat95@gmail.com>
2025-10-27 15:02:14 +0300

    Refactor GET method handling and comments

--- api/index.js
+++ api/index.js
@@ -2,22 +2,23 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  // Sadece GET çalışsın
+  // Sadece GET olsun
   if (req.method !== "GET") {
-    return res.status(405).send("Method Not Allowed");
+    return res
+      .status(405)
+      .send("Method Not Allowed");
   }
 
-  // index.html dosyasını root'tan oku
+  // index.html'i repo root'undan oku
   const filePath = path.join(process.cwd(), "index.html");
 
   try {
     const html = fs.readFileSync(filePath, "utf8");
-
     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(html);
+    return res.status(200).send(html);
   } catch (err) {
     console.error("index.html okunamadı:", err);
-    res
+    return res
       .status(500)
       .send("Beklenmeyen bir hata oldu (index.html bulunamadı).");
   }
