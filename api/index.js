c49c2a2b0e63142909d19798fe23eb4f6d79ee86
MataramaSuko <batuhanpolat95@gmail.com>
2025-10-27 15:44:26 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,18 +2,22 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function handler(req, res) {
+  // sadece GET
   if (req.method !== "GET") {
     return res.status(405).send("Method Not Allowed");
   }
 
+  // repo root'taki index.html dosyasını oku
   const filePath = path.join(process.cwd(), "index.html");
 
   try {
     const html = fs.readFileSync(filePath, "utf8");
     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(html);
+    return res.status(200).send(html);
   } catch (err) {
     console.error("index.html okunamadı:", err);
-    res.status(500).send("Beklenmeyen bir hata oldu (index.html bulunamadı).");
+    return res
+      .status(500)
+      .send("Beklenmeyen bir hata oldu (index.html bulunamadı).");
   }
 };
