c8a9480ed7a23663919ea9c2e83aae3b4d308453
MataramaSuko <batuhanpolat95@gmail.com>
2025-10-27 15:13:32 +0300

    Implement GET handler for serving index.html

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  const filePath = path.join(process.cwd(), "index.html");
+
+  try {
+    const html = fs.readFileSync(filePath, "utf8");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.status(200).send(html);
+  } catch (err) {
+    console.error("index.html okunamadı:", err);
+    res
+      .status(500)
+      .send("Beklenmeyen bir hata oldu (index.html bulunamadı).");
+  }
+};
